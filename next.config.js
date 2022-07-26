724b5f32710eab11ab50af0da57f9297dd3bb732
Kalkidan Behayilu <99049241+kalbehayilu@users.noreply.github.com>
2022-07-26 09:31:00 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 const nextConfig = {
+    reactStrictMode: true,
     images: {
       loader: 'akamai',
-      path: '/',
+      path: '',
     },
-    reactStrictMode: true,
-//     basePath: "/nextjs-pages",
-//     assetPrifix: "/nextjs-pages"
+    basePath: "/nextjs-pages",
+    assetPrifix: "/nextjs-pages"
   }
 
 module.exports = nextConfig;
