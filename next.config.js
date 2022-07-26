346ea33da6f3a77aa574204fb67789a503836a0e
Kalkidan Behayilu <99049241+kalbehayilu@users.noreply.github.com>
2022-07-26 10:38:33 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const nextConfig = {
     reactStrictMode: true,
     images: {
       loader: 'akamai',
-      path: '',
+      path: 'https://hypedriven.github.io',
     },
     basePath: "/Chargr",
     assetPrifix: "/Chargr"
